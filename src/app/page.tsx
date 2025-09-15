080b1f890e5861ecec1ceadd1c30c418b691968c
Arnaud Ferrand <arferrand@gmail.com>
2025-09-15 14:39:08 +0100

    adds a client request example (not recommended)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import opencage from 'opencage-api-client';
 import type { GeocodingRequest } from 'opencage-api-client';
+import Footer from '@/components/Footer';
 
 export default async function Home() {
   // Server side call to OpenCage Geocoding API
@@ -38,8 +39,28 @@ export default async function Home() {
             </div>
           </li>
           <li className="tracking-[-.01em]">
-            Click the <Link href={'/client'}>button</Link> below to see an
-            example using a page rendered on the client-side.
+            Click the button{' '}
+            <Link href={'/client'}>
+              <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+                Client with Server API
+              </code>
+            </Link>{' '}
+            below to see an example using a page rendered on the client-side
+            using a server-side API.
+          </li>
+          <li className="tracking-[-.01em]">
+            Click the button{' '}
+            <Link href={'/wrong'}>
+              <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+                Client with Client API
+              </code>
+            </Link>{' '}
+            below to see an example using a page rendered on the client-side
+            using a client-side request, exposing the API Key.{' '}
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              THIS IS NOT RECOMMENDED
+            </code>
+            .
           </li>
         </ol>
 
@@ -55,7 +76,20 @@ export default async function Home() {
               width={20}
               height={20}
             />
-            Client-Side Example
+            Client with Server API
+          </Link>
+          <Link
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center gap-2  hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href={'/wrong'}
+          >
+            <Image
+              className="dark:invert"
+              src="/globe.svg"
+              alt="Globe icon"
+              width={20}
+              height={20}
+            />
+            Client with Server API
           </Link>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -67,38 +101,7 @@ export default async function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Github
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://opencagedata.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to OpenCage →
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
--- src/app/wrong/page.tsx
+++ src/app/wrong/page.tsx
@@ -3,28 +3,33 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import opencage from 'opencage-api-client';
+// import { geocode } from 'opencage-api-client'; // works as well
 import type { GeocodingResponse } from 'opencage-api-client';
 import Footer from '@/components/Footer';
 
-export default function Client() {
+export default function WrongUsageClientSide() {
   // In production, you should use a server-side API route to hide your API key
   // https://nextjs.org/docs/app/building-your-application/routing/api-routes
 
-  const [serverResult, setServerResult] = useState<GeocodingResponse | null>(
+  const [clientResult, setClientResult] = useState<GeocodingResponse | null>(
     null
   );
   const [loading, setLoading] = useState(false);
 
-  async function handleServerGeocode() {
+  async function handleClientGeocode() {
     setLoading(true);
-    const res = await fetch('/api/geocode', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      // hardcoded coordinates for demo purposes and anyway the test key will always the same result
-      body: JSON.stringify({ q: '51.952659,7.632473' }),
+    // This is a WRONG usage, the API key is exposed to the browser
+    // and can be misused by anyone
+    // NEVER do this in production
+
+    // const res = await geocode({ // works as well
+
+    const res = await opencage.geocode({
+      q: '51.952659,7.632473',
+      key: '6d0e711d72d74daeb2b0bfd2a5cdfdba', // test key. Always the same result
     });
-    const data = await res.json();
-    setServerResult(data);
+    setClientResult(res);
     setLoading(false);
   }
 
@@ -41,16 +46,16 @@ export default function Client() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            This page is rendered client-side, and uses the{' '}
+            This page is rendered client-side and uses the{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               OpenCage Geocoding API
-            </code>
-            , the API Key is{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              hidden
             </code>{' '}
-            from the browser as the geocoding route is called from a server-side
-            API route, click the button below to see the result
+            client-side. The API Key is
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              exposed
+            </code>{' '}
+            in the browser as the geocoding is called from the client-side.
+            Click the button below to see the result.
           </li>
         </ol>
 
@@ -58,7 +63,7 @@ export default function Client() {
           <button
             type="button"
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            onClick={handleServerGeocode}
+            onClick={handleClientGeocode}
             disabled={loading}
           >
             <Image
@@ -68,13 +73,13 @@ export default function Client() {
               width={20}
               height={20}
             />
-            {loading ? 'Loading...' : 'Geocode (Server)'}
+            {loading ? 'Loading...' : 'Geocode (Client)'}
           </button>
-          {serverResult && (
+          {clientResult && (
             <div className="mt-4 text-center">
               <strong>Server-side result:</strong>
               <pre>
-                {JSON.stringify(serverResult.results[0].components, null, 2)}
+                {JSON.stringify(clientResult.results[0].components, null, 2)}
               </pre>
             </div>
           )}
